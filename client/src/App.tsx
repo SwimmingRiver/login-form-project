7bc0606db00965162267f1ccb4885697918cd0fb
swimmingRiver <swimmingr@gmail.com>
2025-01-04 23:34:46 +0900

    feat: hookform react-query 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 import "./App.css";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/Signup";
 
 function App() {
-  return <div className="App">ㅁ</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;

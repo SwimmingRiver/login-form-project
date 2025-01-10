1fba78d73419c68bda35d47078b1bbfb42a3efa9
swimmingRiver <swimmingr@gmail.com>
2025-01-10 22:56:22 +0900

    feat: 회원가입 db 연결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+  ],
 })
 export class AppModule {}

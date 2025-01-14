930225576e6f82c0a3054d79f8a6937f3c1bdfea
swimmingRiver <swimmingr@gmail.com>
2025-01-15 01:25:32 +0900

    feat: 회원 비밀번호 해싱처리

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './models/user.schema';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,13 +13,19 @@ export class UserService {
     private userModel: Model<User>,
   ) {}
   async create(createUserDto: CreateUserDto): Promise<User> {
+    const { useremail, username, password } = createUserDto;
     const existingUser = await this.userModel.findOne({
       useremail: createUserDto.useremail,
     });
     if (existingUser) {
       throw new Error('이미 등록된 이메일입니다.');
     }
-    const createUserData = new this.userModel(createUserDto);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const createUserData = new this.userModel({
+      useremail,
+      username,
+      hashedPassword,
+    });
 
     return createUserData.save();
   }

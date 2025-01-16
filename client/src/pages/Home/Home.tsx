1c493d4383599e988918abc4bb0ea4e87e991706
swimmingRiver <swimmingr@gmail.com>
2025-01-16 21:21:10 +0900

    feat:유저 정보관리 zustand 추가

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import useAuthStore from "../../store/authStore";
 
 function Home() {
+  const { user, isLoggedIn } = useAuthStore();
+  console.log(user);
   return (
     <div>
       <span>Home</span>
+      {isLoggedIn && <span>{user?.username}</span>}
       <Link to={"/login"}>login</Link>
     </div>
   );
